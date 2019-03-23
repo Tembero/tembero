ed8d4145316bb7c9f862b50816bcceeb4698212d
Tembero <48765226+Tembero@users.noreply.github.com>
2019-03-23 15:25:49 +0200

    Update network.js

--- network.js
+++ network.js
@@ -1,18 +1,13 @@
 var network = {
     version: "0.0.1",
     files: [
-        "alpha.png",
-        "alpha_gradient.png",
         "color.js",
-        "color_background.png",
-        "color_slider.png",
         "draw.js",
         "index.html",
         "launch.js",
         "launcher.css",
         "launcher.html",
         "launcher.js",
-        "launcherBackground.png",
         "main.js",
         "package-lock.json",
         "package.json",
